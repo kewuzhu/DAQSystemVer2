2621782d6e1d895736c0776613489ec320f7f42b
yourname <852073346@qq.com>
2024-11-06 10:09:27 +0800

    image buatton

---
+++ DAQSystemVer2/Themes/ImageButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace DAQSystem.Application.Themes
+{
+    internal class ImageButton : System.Windows.Controls.Button
+    {
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register(nameof(ImageSource), typeof(object), typeof(ImageButton));
+
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register(nameof(ImageWidth), typeof(double), typeof(ImageButton));
+
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register(nameof(ImageHeight), typeof(double), typeof(ImageButton));
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register(nameof(IsActive), typeof(bool), typeof(ImageButton));
+
+        public object ImageSource
+        {
+            get => (string)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+
+        public double ImageWidth
+        {
+            get => (double)GetValue(ImageWidthProperty);
+            set => SetValue(ImageWidthProperty, value);
+        }
+
+        public double ImageHeight
+        {
+            get => (double)GetValue(ImageHeightProperty);
+            set => SetValue(ImageHeightProperty, value);
+        }
+
+        public bool IsActive
+        {
+            get => (bool)GetValue(IsActiveProperty);
+            set => SetValue(IsActiveProperty, value);
+        }
+    }
+}
